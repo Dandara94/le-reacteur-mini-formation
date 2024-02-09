e6c57e9d3c54675c1fbe959302147761fd1fc5a5
Dandara94 <dandara.helen56@gmail.com>
2024-02-09 14:24:35 +0100

    add3 exercices addition.js modulo.js number-of-seconds.js

---
+++ number-of-seconds.js
@@ -0,0 +1,7 @@
+const hours = 3; // 3hours
+const minute = 60; // 60s in 1 minute
+const hour = 60; // 60min in 1 hour
+const result = minute * hour; // multiply minute and hour
+const total = total * hours;// multiply my result with the hours 
+console.log(total);
+// => 10800
